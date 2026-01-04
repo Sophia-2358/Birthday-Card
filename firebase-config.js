2b60d7fddd7a4a78b62d42563663424de3c03059
Sophia-2358 <sophiapola.avelinogarcia@gmail.com>
2026-01-04 21:59:30 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -12,3 +12,5 @@ const firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
+
+console.log('Firebase initialized:', firebase.apps.length > 0);
